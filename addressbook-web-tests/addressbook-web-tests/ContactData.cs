662b50273fb7ed9b77917a42b920308d114b666d
aipov <aipov@users.noreply.github.com>
2018-10-02 18:41:27 +0300

    Создан тест Контакты, используются вспомогательные классы

---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class ContactData
+    {
+        private string firstname;
+        private string middlename = "none";
+        private string lastname = "none";
+        private string nickname = "none";
+        
+        private string title = "none";
+        private string company = "none";
+        private string address = "none";
+        private string home = "none";
+        private string mobile = "none";
+        private string work = "none";
+        private string fax = "none";
+        private string email = "none";
+        private string email2 = "none";
+        private string homepage = "none";
+        private string address2 = "none";
+        private string notes = "none";
+        private string phone2 = "none";
+        private string email3 = "none";
+
+        public ContactData(string firstname)
+        {
+            this.firstname = firstname;
+        }
+
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        public string Middlename
+        {
+            get
+            {
+                return middlename;
+            }
+            set
+            {
+                middlename = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+
+        
+         public string Nickname
+        {
+            get
+            {
+                return nickname;
+            }
+            set
+            {
+                nickname = value;
+            }
+        }
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+            
+        public string Company
+            {
+             get
+                {
+                    return company;
+                }
+                set
+                {
+                    company = value;
+                }   
+            }
+        public string Address
+            {
+                get
+                {
+                    return address;
+                }
+                set
+                {
+                    address = value;
+                }
+            }
+        public string Home
+            {
+                get
+                {
+                    return home;
+                }
+                set
+                {
+                    home = value;
+                }
+            }
+        public string Mobile
+            {
+                get
+                {
+                    return mobile;
+                }
+                set
+                {
+                    mobile = value;
+                }
+            }
+        public string Work
+            {
+                get
+                {
+                    return work;
+                }
+                set
+                {
+                    work = value;
+                }
+            }
+        public string Fax
+            {
+                get
+                {
+                    return fax;
+                }
+                set
+                {
+                    fax = value;
+                }
+            }
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+        public string Email2
+        {
+            get
+            {
+                return email2;
+            }
+            set
+            {
+                email2 = value;
+            }
+        }
+        public string Homepage
+        {
+            get
+            {
+                return homepage;
+            }
+            set
+            {
+                homepage = value;
+            }
+        }
+        public string Address2
+        {
+            get
+            {
+                return address2;
+            }
+            set
+            {
+                address2 = value;
+            }
+        }
+        public string Notes
+        {
+            get
+            {
+                return notes;
+            }
+            set
+            {
+                notes = value;
+            }
+        }
+        public string Phone2
+               {
+                   get
+                   {
+                       return phone2;
+                   }
+                   set
+                   {
+                       home = phone2;
+                   }
+               }
+        public string Email3
+        {
+                   get
+                   {
+                       return email3;
+                   }
+                   set
+                   {
+                       email3 = value;
+                   }
+               }
+
+    }
+}
